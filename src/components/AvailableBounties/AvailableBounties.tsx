ebd9afa8b7fb2905ccb72cf358b836c2026bd721
MarsIfeanyi <marschalice@gmail.com>
2023-07-14 22:58:08 +0100

    Create the Mobile Responsiveness for the Page

--- src/components/AvailableBounties/AvailableBounties.tsx
+++ src/components/AvailableBounties/AvailableBounties.tsx
@@ -19,24 +19,25 @@ const AvailableBounties = (props: Props) => {
           </Link>
         </div>
 
-        <div className="overflow-hidden bg-[#0A0A0A] h-screen p-2 mt-10 ">
-          <div className="flex flex-row  justify-between p-4  mx-6 ">
+        {/* Desktop View */}
+        <div className="   bg-[#0A0A0A] h-full p-2 mt-10 mx-6 md:mx-0 ">
+          <div className="flex flex-col md:flex-row  md:justify-between p-4 space-y-1 md:space-y-0 md:mx-6 mx-0 ">
             {/* Members */}
             <div className="flex flex-row gap-1 ">
-              <h2 className="text-white  font-dmSans font-extrabold text-2xl ">
+              <h2 className="text-white  font-dmSans font-extrabold text-xl md:text-2xl ml-4 md:ml-0 ">
                 Available Bounties
               </h2>
             </div>
 
             {/* Search */}
-            <div className="flex  w-1/3  bg-[#0F0F0F] rounded-full items-center  ">
+            <div className="flex w-full md:w-1/3  bg-[#0F0F0F] rounded-full items-center  ">
               <button>
                 <BiSearch className="w-6 h-6 ml-6" />
               </button>
               <input
                 type="text"
                 placeholder="Search"
-                className="w-full  outline-none  text-white py-2 px-4 bg-[#0F0F0F] rounded-full "
+                className="w-full  outline-none  text-white py-1 md:py-2 px-4 bg-[#0F0F0F] rounded-full "
               />
             </div>
           </div>
@@ -45,6 +46,8 @@ const AvailableBounties = (props: Props) => {
             <AvailableBountiesCard />
           </div>
         </div>
+
+        {/* Mobile View */}
       </div>
     </>
   );
