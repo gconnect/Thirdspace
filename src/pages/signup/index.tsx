162fe588054f9c657ca0a4f63d2c6e0526eaaa95
MarsIfeanyi <marschalice@gmail.com>
2023-07-15 00:10:05 +0100

    Changed Font, Reduced Sizes and Text styles and Mobile Responsiveness

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -11,34 +11,34 @@ type Props = {};
 const SigUpPage = (props: Props) => {
   return (
     <>
-      <div className="ml-10 mb-4">
-        <Link href="/">
-          <Image src={logo} alt="logo" width={180} />
-        </Link>
-      </div>
-      <div className=" mx-auto w-[704px] h-[650px] bg-[#0F0F0F] flex flex-col text-white -mt-20 shadow-inner ">
-        <div className=" p-2 rounded">
-          <button className="text-white text-2xl absolute top-8 right-10   "></button>
+      <div className="bg-[#050505] h-full">
+        <div className="ml-10 mb-4">
+          <Link href="/">
+            <Image src={logo} alt="logo" width={180} />
+          </Link>
+        </div>
+        <div className="mx-6 mt-6 md:mx-auto w-[450px] md:w-[500px] h-[600px] bg-[#0F0F0F] flex flex-col text-white md:-mt-12 shadow-inner  ">
+          <div className=" p-2 rounded">
+            <div className="flex flex-col mt-5 mx-[48px]">
+              <h2 className="text-white font-bold text-2xl text-center font-dmSans">
+                Sign Up
+              </h2>
 
-          <div className="flex flex-col mt-5 mx-[48px]">
-            <h2 className="text-white font-bold text-4xl text-center font-nexa">
-              Sign Up
-            </h2>
+              <button className="font-dmSans h-[50px] mt-6 flex flex-row gap-4 items-center justify-center bg-[#141414] ">
+                <Image src={gmailLogo} alt="gmailLogo" width={40} />
+                <span className="font-medium text-lg"> Gmail</span>
+              </button>
 
-            <button className=" font-nexa h-[50px] mt-10 flex flex-row gap-4 items-center justify-center bg-[#141414] ">
-              <Image src={gmailLogo} alt="gmailLogo" width={40} />
-              <span className="text-2xl"> Gmail</span>
-            </button>
+              <p className="mt-[24px] mx-auto items-center justify-center text-[#999999]">
+                Or
+              </p>
 
-            <p className="mt-[24px] mx-auto items-center justify-center text-[#999999]">
-              Or
-            </p>
-
-            <div className="mt-6">
-              <SignUpForm />
+              <div className="mt-6">
+                <SignUpForm />
+              </div>
             </div>
+            {/*  */}
           </div>
-          {/*  */}
         </div>
       </div>
     </>
