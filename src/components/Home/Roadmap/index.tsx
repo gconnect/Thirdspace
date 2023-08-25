26517c27437d27fce6d87c0c3f9ac3c21d0e12ad
gconnect <agatevureglory@gmail.com>
2023-08-25 19:23:26 +0100

    fix partner section add margin

--- src/components/Home/Roadmap/index.tsx
+++ src/components/Home/Roadmap/index.tsx
@@ -8,8 +8,8 @@ const Roadmap = () => {
         <div className="  bg-neutral-800">
             <div className="text-white text-center">
                 <h3 className="text-3xl py-8">Roadmap</h3>
-                <p className="md:px-16">Our roadmap takes us through a series of exciting phases, each designed to bring us closer to our vision.</p>
-                    <p>With each step, we’re making the process easy for Startups and Bounty hunters</p>
+                <p className="md:px-16 px-8">Our roadmap takes us through a series of exciting phases, each designed to bring us closer to our vision.</p>
+                    <p className="md:px-16 px-8">With each step, we’re making the process easy for Startups and Bounty hunters</p>
                     </div>
                     <div id="roadmap" className="mt-8  lg:mx-16 xl:mx-16 md:mx-16 mx-8">
                     {/* <Image className="" src={roadmap}  alt="bounty banner"/> */}
