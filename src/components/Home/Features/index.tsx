396603700304e379d7cb9dda5b36d1bbd8662fd4
gconnect <agatevureglory@gmail.com>
2023-08-25 19:12:35 +0100

    fix feature section responsiveness

--- src/components/Home/Features/index.tsx
+++ src/components/Home/Features/index.tsx
@@ -3,7 +3,7 @@ import FeatureCard from "./FeatureCard";
 
 const Features = () => {
     return ( 
-        <div id="features" className=" mt-8 sm:mx-4 lg:mt-36 xl:mt-36 md:mt-36 lg:mx-48 xl:mx-48 md:mx-16">
+        <div id="features" className=" mt-8 sm:mx-4 lg:mt-36 xl:mt-36 md:mt-36 lg:mx-24 xl:mx-48 md:mx-8">
             <h1 className="text-center  text-white  font-extrabold lg:my-24 xl:my-24 md:my-24 mt-8 text-3xl">Features</h1>
             <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 md:grid-cols-2 justify-items-center mx-8 gap-16">
                 {FeatureItems.map((item, index) => (
