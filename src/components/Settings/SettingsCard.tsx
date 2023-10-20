19cfc065c9e4dffa032f348702be6b194d57ae0e
dorteks <ayodelealayemi@gmail.com>
2023-10-20 17:02:46 +0100

    added account settings page

--- src/components/Settings/SettingsCard.tsx
+++ src/components/Settings/SettingsCard.tsx
@@ -12,12 +12,15 @@ const SettingCard = (props: Props) => {
         <div className="flex justify-between flex-row ">
           <div className="flex flex-col">
             <h2>Bounty space Logo/Image</h2>
-            <div className="w-36 h-36 rounded-full border-[#999999] border-2  flex items-center  justify-center mt-[16px] ">
+            <div className="w-36 h-36 rounded-full border-[#999999] border-2  flex items-center  justify-center mt-[16px]">
               <button className="text-sm">Click to upload Photo</button>
             </div>
           </div>
 
-          <button  className="items-center rounded-full flex flex-row bg-[#B2000029] border border-[#B20000] w-[260px] h-[48px] justify-center gap-1 p-2 font-nexa font-normal  " onClick={() => setShowModal(true)}>
+          <button
+            className="items-center rounded-full flex flex-row bg-[#B2000029] border border-[#B20000] w-[260px] h-[48px] justify-center gap-1 p-2 font-nexa font-normal  "
+            onClick={() => setShowModal(true)}
+          >
             <p>Delete Bounty Space</p>
 
             <div className="RemoveIcon-container">
@@ -28,13 +31,12 @@ const SettingCard = (props: Props) => {
         <div className="mt-10">
           <SettingsCardForm />
         </div>
-        
       </div>
-      <DeleteSettings
-          isVisible={showModal}
-          onClose={() => setShowModal(false)}
-        ></DeleteSettings>
 
+      <DeleteSettings
+        isVisible={showModal}
+        onClose={() => setShowModal(false)}
+      ></DeleteSettings>
     </div>
   );
 };
