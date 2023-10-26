a9b6cb59e3bc760a793bc13624d85f567e5419e0
gconnect <agatevureglory@gmail.com>
2023-10-26 01:03:36 +0100

    fix conflict on available bounties card

--- src/components/AvailableBounties/AvailableBountiesCard.tsx
+++ src/components/AvailableBounties/AvailableBountiesCard.tsx
@@ -6,27 +6,28 @@ import { BASE_URL } from "@/utils/constants";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-type Props = {
-  selectedBounty: Bounty | null;
-};
 
 type Bounty = {
   id: string;
-  image: string;
+  profileImage: string; // Corrected property name
   title: string;
-  startDate: string;
-  endDate: string;
-  description: string;
-  reward: string;
+  start_date: string; // Corrected property name
+  end_date: string;
+  bounty_description: string; // Corrected property name
+  amount: string;
+};
+
+type Props = {
+  selectedBounty: Bounty | null;
 };
 
 const AvailableBountiesCard = (props: any) => {
   const [showModal, setShowModal] = useState(false);
   const [selectedBounty, setSelectedBounty] = useState<Bounty | null>(null);
-  const [availableBounties, setAvailableBounties] = useState<any[]>([]);
-  const { data: session, status } = useSession()
-  console.log(status)
+  const [availableBounties, setAvailableBounties] = useState<Bounty[]>([]); // Specify the type
 
+  const { data: session, status } = useSession();
+  console.log(status);
 
   const handleBountyClick = (selectedBounty: Bounty) => {
     setShowModal(true);
@@ -34,8 +35,7 @@ const AvailableBountiesCard = (props: any) => {
   };
 
   const getAvailableBounties = useCallback(async () => {
-
-    try{
+    try {
       const res = await axios.get(`${BASE_URL}/bounties`, {
         headers: {
           Authorization: `Bearer ${session?.user.token}`,
@@ -53,59 +53,19 @@ const AvailableBountiesCard = (props: any) => {
   }, [getAvailableBounties]);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2  ">
-      {availableBounties && availableBounties.length == 0 ? 
-      <div>Opps there is currently no bounty. Check back later!</div> 
-      :availableBounties &&
-       availableBounties.map((item: any) => (
-        <div
-          key={item.id}
-          className="text-white"
-          onClick={() => handleBountyClick(item)}
-        >
-          {/* Desktop View */}
-          <div className="hidden md:flex border-[#1F1F1F] border  mt-4 mx-6 relative ">
-            <div className="flex flex-row gap-4 p-6 cursor-pointer">
-              <Image
-                src={item.profileImage}
-                alt="bountyImage"
-                width={100}
-                className=""
-              />
-
-              <div className="flex flex-col mt-3">
-                <h2 className="font-dmSans font-bold text-base">
-                  {item.title}
-                </h2>
-                <div className="flex flex-row justify-between font-dmSans text-xs my-2 mr-6">
-                  <p>
-                    <span className="text-[#999999]">Start Date: </span>
-                    {item.start_date}
-                  </p>
-                  <p>
-                    <span className="text-[#999999]">End Date:</span>
-                    {item.end_date}
-                  </p>
-                </div>
-
-                <p className="font-dmSans  font-normal text-xs mb-2 text-[#999999]">
-                  {item.bounty_description}
-                </p>
-              </div>
-
-              <div className="flex justify-center w-[120px] h-[30px] bg-[#1F1F1F] items-center absolute bottom-1 right-6">
-                <p className="font font-bold text-xs">
-                  {item.amount}
-                </p>
-              </div>
-            </div>
-          </div>
-
-            {/* Mobile View  */}
-
-          <div className=" flex md:hidden  mt-4  ">
-            <div className="flex flex-col  p-6 cursor-pointer">
-              <div className="flex flex-row gap-4 ">
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      {availableBounties.length === 0 ? (
+        <div>Opps, there is currently no bounty. Check back later!</div>
+      ) : (
+        availableBounties.map((item: Bounty) => (
+          <div
+            key={item.id}
+            className="text-white"
+            onClick={() => handleBountyClick(item)}
+          >
+            {/* Desktop View */}
+            <div className="hidden md:flex border-[#1F1F1F] border mt-4 mx-6 relative">
+              <div className="flex flex-row gap-4 p-6 cursor-pointer">
                 <Image
                   src={item.profileImage}
                   alt="bountyImage"
@@ -113,11 +73,11 @@ const AvailableBountiesCard = (props: any) => {
                   className=""
                 />
 
-                <div className="flex flex-col ">
-                  <h2 className="font-dmSans font-medium text-base">
+                <div className="flex flex-col mt-3">
+                  <h2 className="font-dmSans font-bold text-base">
                     {item.title}
                   </h2>
-                  <div className="flex flex-col font-dmSans font-medium text-xs space-y-1 mt-2">
+                  <div className="flex flex-row justify-between font-dmSans text-xs my-2 mr-6">
                     <p>
                       <span className="text-[#999999]">Start Date: </span>
                       {item.start_date}
@@ -127,16 +87,52 @@ const AvailableBountiesCard = (props: any) => {
                       {item.end_date}
                     </p>
                   </div>
+
+                  <p className="font-dmSans font-normal text-xs mb-2 text-[#999999]">
+                    {item.bounty_description}
+                  </p>
+                </div>
+
+                <div className="flex justify-center w-[120px] h-[30px] bg-[#1F1F1F] items-center absolute bottom-1 right-6">
+                  <p className="font font-bold text-xs">{item.amount}</p>
                 </div>
               </div>
-              <p className="font-dmSans mt-4 font-normal text-xs mb-2 text-[#999999]">
-                {item.bounty_description}
-              </p>
+            </div>
 
-              <div className="flex justify-center w-full h-[30px] bg-[#1F1F1F] items-center mt-2 ">
-                <p className="font font-bold text-xs">
-                  {item.amount}
+            {/* Mobile View */}
+            <div className="flex md:hidden mt-4">
+              <div className="flex flex-col p-6 cursor-pointer">
+                <div className="flex flex-row gap-4">
+                  <Image
+                    src={item.profileImage}
+                    alt="bountyImage"
+                    width={100}
+                    className=""
+                  />
+
+                  <div className="flex flex-col">
+                    <h2 className="font-dmSans font-medium text-base">
+                      {item.title}
+                    </h2>
+                    <div className="flex flex-col font-dmSans font-medium text-xs space-y-1 mt-2">
+                      <p>
+                        <span className="text-[#999999]">Start Date: </span>
+                        {item.start_date}
+                      </p>
+                      <p>
+                        <span className="text-[#999999]">End Date:</span>
+                        {item.end_date}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <p className="font-dmSans mt-4 font-normal text-xs mb-2 text-[#999999]">
+                  {item.bounty_description}
                 </p>
+
+                <div className="flex justify-center w-full h-[30px] bg-[#1F1F1F] items-center mt-2">
+                  <p className="font font-bold text-xs">{item.amount}</p>
+                </div>
               </div>
             </div>
           </div>
